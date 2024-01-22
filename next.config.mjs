3dfcf564314bdcaf53a650063db0f746b4c43806
Johan Villamil <johinsdev@gmail.com>
2024-01-22 18:52:14 -0500

    Add configuration files and dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  swcMinify: true,
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    windowHistorySupport: true,
+    clientRouterFilter: false,
+  },
+};
 
 export default nextConfig;
