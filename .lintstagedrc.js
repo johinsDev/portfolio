d061219da02a66529b0605743ce86d3df9b85364
Johan Villamil <johinsdev@gmail.com>
2024-01-22 18:57:16 -0500

    feat: 🎸 Add docker-compose.yml for MySQL service

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,15 +1,15 @@
-const path = require("path")
+const path = require("path");
 
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
-    .join(" --file ")}`
+    .join(" --file ")}`;
 
 const buildPrettierCommand = (filenames) =>
-  `prettier --write ${filenames
+  `pretty-quick ${filenames
     .map((f) => path.relative(process.cwd(), f))
-    .join(" ")}`
+    .join(" ")}`;
 
 module.exports = {
   "*.{js,jsx,ts,tsx}": [buildEslintCommand, buildPrettierCommand],
-}
+};
