811d87d797ec2ab4cff7ece7835f2ad9411b373c
Johan Villamil <johinsdev@gmail.com>
2024-01-23 12:05:08 -0500

    chore: :fire: update turbo.json, .husky/pre-commit, src/app/page.tsx, and package.json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>FAST</h1>
+      <h1>FASTER</h1>
     </main>
   );
 }
