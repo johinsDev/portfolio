2dab06c74081034479ee2b98fb266a274e78b40e
Johan Villamil <johinsdev@gmail.com>
2024-01-23 12:47:44 -0500

    feat(home): :lipstick: update styles home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
+import { cn } from "@/lib/utils";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main
+      className={cn(
+        "flex min-h-screen flex-col items-center justify-between p-24",
+        "bg-gradient-to-b from-blue-400 to-blue-500",
+        "text-white",
+        "font-bold",
+        "text-6xl",
+        "space-y-8",
+        "text-center",
+        "transition duration-500 ease-in-out hover:-translate-y-1 hover:scale-110",
+      )}
+    >
       <h1>FASTER</h1>
     </main>
   );
